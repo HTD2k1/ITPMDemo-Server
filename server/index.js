44c85b74b02ad58197a79701b87a0e5fc1f11d9b
HTD2k1 <58422272+HTD2k1@users.noreply.github.com>
2022-06-09 15:14:12 +0700

    implement search functionality on server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/user.js';
-
+import User from './models/user.js'
 const app = express();
 app.use(express.json({limit: "30mb", extended:true}));
 app.use(express.urlencoded({limit: "30mb", extended:true}))
@@ -15,10 +15,11 @@ app.use('/user',userRoutes);
 // const CONNECTION_URL = "mongodb+srv://admin:admin@webappproject.p2q1m.mongodb.net/WebAppDB?retryWrites=true&w=majority";
 const CONNECTION_URL = "mongodb+srv://admin:admin1@cluster0.fm9jr.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology : true})
+await mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology : true})
 .then(()=>{ app.listen(PORT, ()=>{
   console.log(`Connected to database and running on ${PORT}`);
 }) })
 .catch((error)=>{
   console.log(error.message);
 })
+
