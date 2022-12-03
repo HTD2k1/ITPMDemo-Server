942ecd628dd4afbe5df44eb973bff56b68f2c555
HTD2k1 <58422272+HTD2k1@users.noreply.github.com>
2022-12-03 09:22:07 +0700

    change port

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use('/user',userRoutes);
 
 // const CONNECTION_URL = "mongodb+srv://admin:admin@webappproject.p2q1m.mongodb.net/WebAppDB?retryWrites=true&w=majority";
 const CONNECTION_URL = "mongodb+srv://admin:admin1@cluster0.fm9jr.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 await mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology : true})
 .then(()=>{ app.listen(PORT, ()=>{
   console.log(`Connected to database and running on ${PORT}`);
